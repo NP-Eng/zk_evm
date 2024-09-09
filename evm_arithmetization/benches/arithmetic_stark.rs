6737522b9eecf7f4fa2cf2c0522baf6b701bd855
Antonio Mejías Gil <anmegi.95@gmail.com>
2024-09-09 11:55:13 +0200

    expanded arithmetic benchmarks, explained relation between number of operations and polynomial degree
    Co-authored-by: Cesar Descalzo <Cesar199999@users.noreply.github.com>

--- evm_arithmetization/benches/arithmetic_stark.rs
+++ evm_arithmetization/benches/arithmetic_stark.rs
@@ -48,26 +48,139 @@ where
     ];
 
     for (op, op_str) in ops.zip(op_strs) {
-        bench_arithmetic_operation(c, op, op_str, config.clone(), &config_str);
+        bench_arithmetic_operations(c, vec![op], op_str, config.clone(), &config_str);
     }
 }
 
-fn bench_arithmetic_operation(
+// The lookup which checks for correct base-2^16 representation of operands
+// occurs in the trace table, which therefore needs to have full column
+// dedicated to the lookup table 0, 1, ..., 2^16 - 1. This explains the minimum
+// number of rows (i.e. interpolating-polynomial degree plus 1) in the trace of
+// the Arithmetic STARK. We test that performing <= 2^16 binary operations
+// (which require one row each) results in a 2^16-row table, and performing
+// 2^16 + 1 binary operations already requires a 2^17-row table
+fn bench_binary_large<const D: usize, F>(c: &mut Criterion)
+where
+    F: RichField + Extendable<D>,
+{
+    let (config_str, config) = stark_config!(4, 5);
+
+    let ops_half_below = (0..(1 << 15))
+        .map(|_| binary_operations!(Add))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(
+        c,
+        ops_half_below,
+        "Add (half below)",
+        config.clone(),
+        &config_str,
+    );
+
+    let ops_exact = (0..(1 << 16))
+        .map(|_| binary_operations!(Add))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(c, ops_exact, "Add (exact)", config.clone(), &config_str);
+
+    let ops_just_above = (0..(1 << 16) + 1)
+        .map(|_| binary_operations!(Add))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(
+        c,
+        ops_just_above,
+        "Add (just above)",
+        config.clone(),
+        &config_str,
+    );
+
+    let ops_twice_above = (0..(1 << 17))
+        .map(|_| binary_operations!(Add))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(c, ops_twice_above, "Add (twice above)", config, &config_str);
+}
+
+// (Cf. explanation to `bench_binary_large`). We test that performing <= 2^15
+// ternary operations (which require two rows each) results in a 2^16-row table,
+// and performing 2^15 + 1 ternary operations already requires a 2^17-row table
+fn bench_ternary_large<const D: usize, F>(c: &mut Criterion) {
+    let (config_str, config) = stark_config!(4, 5);
+
+    let ops_half_below = (0..(1 << 14))
+        .map(|_| ternary_operations!(AddMod))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(
+        c,
+        ops_half_below,
+        "AddMod (half below)",
+        config.clone(),
+        &config_str,
+    );
+
+    let ops_exact = (0..(1 << 15))
+        .map(|_| ternary_operations!(AddMod))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(c, ops_exact, "AddMod (exact)", config.clone(), &config_str);
+
+    let ops_just_above = (0..(1 << 15) + 1)
+        .map(|_| ternary_operations!(AddMod))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(
+        c,
+        ops_just_above,
+        "AddMod (just above)",
+        config.clone(),
+        &config_str,
+    );
+
+    let ops_twice_above = (0..(1 << 16))
+        .map(|_| ternary_operations!(AddMod))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    bench_arithmetic_operations(
+        c,
+        ops_twice_above,
+        "AddMod (twice above)",
+        config,
+        &config_str,
+    );
+}
+
+fn bench_arithmetic_operations(
     c: &mut Criterion,
-    op: Operation,
+    ops: Vec<Operation>,
     op_str: &str,
     config: StarkConfig,
     config_str: &str,
 ) {
     let stark = ArithmeticStark::<F, 2>::default();
     let tag = format!("ArithmeticStark ({:?}) with {}", op_str, config_str);
-    let trace = stark.generate_trace(vec![op]);
+    let trace = stark.generate_trace(ops);
+    println!("Trace length (number of polys): {}", trace.len());
+    println!(
+        "Trace width (number of values per poly): {}",
+        trace[0].len()
+    );
     bench_stark::<F, C, S, D>(c, stark, trace, config, &tag);
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = bench_arithmetic_stark::<D, F>
+    targets = bench_arithmetic_stark::<D, F>, bench_binary_large::<D, F>, bench_ternary_large::<D, F>
 }
+
 criterion_main!(benches);
